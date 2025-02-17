be11391c753d977b1f2183b2f88e8292bacc39de
Ankit Panghal <ankitpanghal.work@gmail.com>
2025-02-17 17:43:33 +0530

    added type safety

--- src/App.tsx
+++ src/App.tsx
@@ -18,17 +18,17 @@ const config = {
     next: 'yellow',
   },
 };
-
+type light = 'red' | 'yellow' | 'green'
 const App = () => {
-  const [currentActiveLight, setCurrentActiveLight] = useState('red');
-  const [currentDuration, setCurrentDuration] = useState(config[currentActiveLight].duration);
+  const [currentActiveLight, setCurrentActiveLight] = useState<light>('red');
+  const [currentDuration, setCurrentDuration] = useState(config['red'].duration);
 
   useEffect(() => {
-    let timerId;
+    let timerId: number | undefined;
     if (currentDuration <= 0) {
       clearInterval(timerId);
-      setCurrentActiveLight(config[currentActiveLight].next);
-      setCurrentDuration(Number(config[config[currentActiveLight].next].duration));
+      setCurrentActiveLight(config[currentActiveLight].next as light);
+      setCurrentDuration(config[config[currentActiveLight].next as light].duration);
     } else {
       timerId = setInterval(() => {
         setCurrentDuration((prevDuration) => prevDuration - 1000);
@@ -42,7 +42,7 @@ const App = () => {
       <div className=" flex flex-col items-center justify-center gap-4 bg-zinc-900 w-48 h-96 rounded-[40px] p-6">
       {Object.keys(config).map((light) => (
           <div
-            key={config[light].id}
+            key={config[light as light].id}
             className='w-24 h-24 rounded-full bg-gray-500'
             style={{ background: currentActiveLight === light ? light : '' }}
           ></div>
